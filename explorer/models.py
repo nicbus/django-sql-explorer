31b0987f82957a4b76942e1ea7bce3cf616fc955
pietro <p.pilolli@trentinosistemi.com>
2014-10-15 11:05:18 +0200

    Add open data flag to query model

--- explorer/models.py
+++ explorer/models.py
@@ -21,6 +21,7 @@ class Query(models.Model):
     aggregations =  models.TextField(null=True, blank=True)
     filters =  models.TextField(null=True, blank=True)
     agg_filters =  models.TextField(null=True, blank=True)
+    open_data = models.BooleanField(default=False)
 
     params = {}
 
